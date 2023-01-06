18a1ca75f55366425a8223e1b7be0430c1f99000
r1411 <roma00055@gmail.com>
2023-01-06 20:02:37 +0300

    Лаб. 1, Задание 2.3

--- Lab1/Task2/task2.rb
+++ Lab1/Task2/task2.rb
@@ -22,7 +22,17 @@ def less_than_3_cnt(number)
   result
 end
 
+# 3. Количество чисел, не являющихся делителями исходного числа, не взамнопростых с ним и взаимно простых с суммой простых цифр этого числа
+def cool_numbers_cnt(number)
+  result = 0
+  prime_sum = 0
+  number.digits.each { |dig| prime_sum += dig if prime?(dig) }
+  number.downto(2).each { |i| result += 1 unless (number % i).zero? || i.gcd(number) == 1 || i.gcd(prime_sum) != 1 }
+  result
+end
+
 puts 'Введите число:'
 x = $stdin.gets.to_i
 puts "Сумма непростых делителей: #{sum_np_div(x)}"
 puts "Количество цифр, меньших 3: #{less_than_3_cnt(x)}"
+puts "Количество чисел задания №3: #{cool_numbers_cnt(x)}"
