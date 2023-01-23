a7cc7d76588f099a934c90ccc909894f89bff28e
r1411 <roma00055@gmail.com>
2023-01-23 17:50:41 +0300

    Лаб. 1, Задание 4 (Метод 30)

--- Lab1/Task4/task4.rb
+++ Lab1/Task4/task4.rb
@@ -9,6 +9,7 @@ puts arr.inspect
 puts 'Выберите действие:'
 puts "\t1. Циклический сдвиг на 3 влево"
 puts "\t2. Элементы до первого минимального"
+puts "\t3. Проверить лок. максимум по индексу"
 
 def method6(arr)
   arr.rotate(3)
@@ -19,7 +20,15 @@ def method18(arr)
   arr[0, arr.index(stop_elem)]
 end
 
-methods = %i[method6 method18]
+def method30(arr)
+  puts 'Индекс проверки лок. максимума:'
+  test_idx = gets.chomp.to_i
+  return false unless test_idx.between?(1, arr.length - 2)
+
+  arr[test_idx - 1] <= arr[test_idx] and arr[test_idx + 1] <= arr[test_idx]
+end
+
+methods = %i[method6 method18 method30]
 option = gets.chomp.to_i
 unless option.between?(1, methods.length)
   puts 'Неизвестный метод'
