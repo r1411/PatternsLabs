fccea2758631a0ddfc392e37516cabce91e7073d
r1411 <roma00055@gmail.com>
2023-01-28 16:04:03 +0300

    Исправление критического недочета

--- Lab2/Task1/main.rb
+++ Lab2/Task1/main.rb
@@ -3,9 +3,9 @@
 require_relative 'student'
 
 student1 = Student.new('Иванов', 'Иван', 'Иванович')
-student2 = Student.new('Ночной', 'Поджог', 'Сараев', id: 1, telegram: 'nightfire')
-student3 = Student.new('Красивый', 'Рулон', 'Обоев', phone: '79997775533', email: 'wallpapers@mail.ru', git: 'roboyev')
-student4 = Student.new('Мирный', 'Косяк', 'Пингвинов', id: 3, phone: '79990001122', telegram: 'pngn', email: 'antarctica@gmail.com', git: 'iampngn')
+student2 = Student.new('Сараев', 'Поджог', 'Равшанович', id: 1, telegram: 'nightfire')
+student3 = Student.new('Обоев', 'Рулон', 'Джамшутович', phone: '79997775533', email: 'wallpapers@mail.ru', git: 'roboyev')
+student4 = Student.new('Пингвинов', 'Косяк', 'Олегович', id: 3, phone: '79990001122', telegram: 'pngn', email: 'antarctica@gmail.com', git: 'iampngn')
 
 puts student1
 puts student2
